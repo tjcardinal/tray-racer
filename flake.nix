f67f27611f0b1c5e21e3b84cb36e48fe5ca2711a
Tyler Cardinal <83625450+tjcardinal@users.noreply.github.com>
2023-07-14 23:10:31 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,12 +4,11 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
-  let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-  in
-  {
-    devShells."x86_64-linux".default = pkgs.mkShell {
-      buildInputs = with pkgs; [ cargo rustc ];
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default =
+        pkgs.mkShell { buildInputs = with pkgs; [ cargo rustc ]; };
     };
-  };
 }
