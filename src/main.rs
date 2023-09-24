0c1627c6048c5d2eb5a13c035dd4a41c3fb48f47
Tyler Cardinal <83625450+tjcardinal@users.noreply.github.com>
2023-09-23 19:44:37 -0500

    Add more spheres

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod ray;
 mod sphere;
 mod vec3;
 
+use rand::Rng;
 use std::{f64::consts::PI, rc::Rc};
 
 use camera::Camera;
@@ -17,52 +18,74 @@ use sphere::Sphere;
 use vec3::{Point3, Vec3};
 
 fn main() {
-    let material_ground = Rc::new(Lambertain::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Rc::new(Lambertain::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Rc::new(Dielectric::new(1.5));
-    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
+    let mut world: Vec<Box<dyn Hittable>> = vec![];
 
-    let world: Vec<Box<dyn Hittable>> = vec![
-        Box::new(Sphere::new(
-            Point3::new(0.0, -100.5, -1.0),
-            100.0,
-            material_ground,
-        )),
-        Box::new(Sphere::new(
-            Point3::new(0.0, 0.0, -1.0),
-            0.5,
-            material_center,
-        )),
-        Box::new(Sphere::new(
-            Point3::new(-1.0, 0.0, -1.0),
-            0.5,
-            material_left.clone(),
-        )),
-        Box::new(Sphere::new(
-            Point3::new(-1.0, 0.0, -1.0),
-            -0.4,
-            material_left,
-        )),
-        Box::new(Sphere::new(
-            Point3::new(1.0, 0.0, -1.0),
-            0.5,
-            material_right,
-        )),
-    ];
+    let ground_material = Rc::new(Lambertain::new(Color::new(0.5, 0.5, 0.5)));
+    world.push(Box::new(Sphere::new(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
+    )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let center = Point3::new(
+                a as f64 + 0.9 * rand::random::<f64>(),
+                0.2,
+                b as f64 + 0.9 * rand::random::<f64>(),
+            );
+            if 0.9 < (center - Point3::new(4.0, 0.2, 0.0)).length() {
+                let material: Rc<dyn Material> = match rand::random::<f64>() {
+                    x if x < 0.8 => {
+                        let albedo = Color::random() * Color::random();
+                        Rc::new(Lambertain::new(albedo))
+                    }
+                    x if x < 0.95 => {
+                        let albedo = Color::random_range(0.5, 1.0);
+                        let fuzz = rand::thread_rng().gen_range(0.5..1.0);
+                        Rc::new(Metal::new(albedo, fuzz))
+                    }
+                    x => Rc::new(Dielectric::new(1.5)),
+                };
+                world.push(Box::new(Sphere::new(center, 0.2, material)));
+            }
+        }
+    }
+
+    let material1 = Rc::new(Dielectric::new(1.5));
+    world.push(Box::new(Sphere::new(
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
+    )));
+
+    let material2 = Rc::new(Lambertain::new(Color::new(0.4, 0.2, 0.1)));
+    world.push(Box::new(Sphere::new(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
+    )));
+
+    let material3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    world.push(Box::new(Sphere::new(
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
+    )));
 
     let mut cam = Camera::new();
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.width = 400;
-    cam.samples_per_pixel = 100;
+    cam.width = 600;
+    cam.samples_per_pixel = 50;
     cam.max_depth = 50;
 
     cam.vfov = 20.0;
-    cam.lookfrom = Point3::new(-2.0, 2.0, 1.0);
-    cam.lookat = Point3::new(0.0, 0.0, -1.0);
+    cam.lookfrom = Point3::new(13.0, 2.0, 3.0);
+    cam.lookat = Point3::new(0.0, 0.0, 0.0);
     cam.vup = Vec3::new(0.0, 1.0, 0.0);
 
-    cam.defocus_angle = 10.0;
-    cam.focus_dist = 3.4;
+    cam.defocus_angle = 0.6;
+    cam.focus_dist = 10.0;
 
     cam.render(&world);
 }
